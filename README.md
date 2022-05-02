cf60499c81525561c549885666f101952a691325
ajadi.paul <ajadi.ololade@gmail.com>
2022-05-02 16:58:36 +0100

    Updating Readme Docs

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-#  ./vendor/bin/phpunit
+## Estate Intel Backend API Task
+    - AJADI, Paul Ololade
+    -   [ajadi.ololade@gmail.com]
 
-# php artisan migrate:fresh --seed
+## Instructions To Get Started
+
+1. Clone to your machine.
+
+2. Create a DB with MariaDB or MySqlDb. 
+
+    a. Copy and rename the .env.example file as .env
+
+3. Run the following commands from the project root
+
+    a. `smile`
+    
+    b. `php artisan key:generate`
+    
+    c. `php artisan migrate:fresh --seed`
+
+    d. `composer install`
+    
+4. Run `php artisan serve` to run the server
+    - Navigate to http://127.0.0.1:8000/api/documentation
+
+## Run tests
+    - From the root folder run `./vendor/bin/phpunit`
--- tests/Unit/BookTest.php
+++ tests/Unit/BookTest.php
@@ -8,6 +8,7 @@ use App\Http\Resources\BooksResource;
 use App\Models\Books;
 use Database\Factories\booksFactory;
 use Tests\TestCase;
+use GuzzleHttp\Client as client;
 
 class BookTest extends TestCase
 {
@@ -86,22 +87,4 @@ class BookTest extends TestCase
   
     }
 
-    public function testFetchExternalAPIBooks()
-    {
-        $response = $this->json('GET', env('API_BASE_URL'));
-        $response->assertStatus(200);
-        $response->assertJsonStructure([
-            "data" => [
-                [
-                    'name',
-                    'isbn',
-                    'authors',
-                    'number_of_pages',
-                    'publisher',
-                    'country',
-                    'release_date',
-                ]
-            ]
-        ]);
-    }
 }
